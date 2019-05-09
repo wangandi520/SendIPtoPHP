65d60c9cd9bb9467808a0a31b685159b89de1136
wangandi520 <30204977@qq.com>
2019-05-09 11:08:40 +0800

    sh

--- updateip.sh
+++ updateip.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-
-curl ifconfig.me > /andy/github/myddns/ip
-oldip=$(cat /andy/github/myddns/oldip)
-ip=$(cat /andy/github/myddns/ip)
-#echo $ip
-#echo $oldip
-if [ "$oldip" = "$ip" ];then
-	echo "ip not changed"
-else
-	echo "ip changed"
-	git commit -m "changed ip" ip
-	git push origin master
-fi
-cp /andy/github/myddns/ip /andy/github/myddns/oldip
+myip=$(cat /andy/github/myddns/ip)
+echo $myip
+git add /andy/github/myddns/ip
+git commit -m "changed ip"
+git push origin master
